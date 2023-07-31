51b56f206a219f3861342683a1afa16ab7a1da0a
LionelROLLAND <lionel.rolland@protonmail.com>
2023-08-01 01:07:28 +0200

    [edit] Add simple tasks definitions.

--- project/task.py
+++ project/task.py
@@ -4,11 +4,13 @@ from math import ceil
 from typing import Callable, Union
 
 
+# pylint: disable=too-many-arguments
 def general_weight_fun(
     _: int,
     n_coloc_days: float,
-    min_prop: float,
     proportional: bool = True,
+    min_prop: float = 0,
+    min_nb_coloc: int = 0,
     weight_per_coloc: Union[float, None] = None,
     total_weight: Union[float, None] = None,
 ) -> tuple[int, float]:
@@ -19,7 +21,7 @@ def general_weight_fun(
     The returned tediousness is per coloc.
     """
     n_virt_coloc = n_coloc_days / 7
-    coloc_needed = max(1, int(ceil(min_prop * n_virt_coloc)))
+    coloc_needed = max(1, int(ceil(min_prop * n_virt_coloc)), min_nb_coloc)
     match proportional:
         case True:
             assert isinstance(weight_per_coloc, float)  # nosec
@@ -31,8 +33,9 @@ def general_weight_fun(
 
 
 def weight_fun(
-    min_prop: float,
     proportional: bool = True,
+    min_prop: float = 0,
+    min_nb_coloc: int = 0,
     weight_per_coloc: Union[float, None] = None,
     total_weight: Union[float, None] = None,
 ) -> Callable[[int, float], tuple[int, float]]:
@@ -76,10 +79,30 @@ def weight_fun(
         _,
         n_coloc_days=n_coloc_days,
         min_prop=min_prop,
+        min_nb_coloc=min_nb_coloc,
         proportional=proportional,
         weight_per_coloc=weight_per_coloc,
         total_weight=total_weight,
     )
+
+
+def need_weekend(available_days: list[bool]) -> bool:
+    """Return true if the week-end is available in the available days defined by the argument."""
+    return available_days[-1] and available_days[-2]
+
+
+def less_than_n_consecutive_days_absent(
+    available_days: list[bool], n_days: int
+) -> bool:
+    """Return True if the available days contain less than n consecutive absent days."""
+    occ = -1
+    max_absent_days = 0
+    for k, available in enumerate(available_days):
+        if available:
+            max_absent_days = max(max_absent_days, k - occ - 1)
+            occ = k
+    max_absent_days = max(max_absent_days, 7 - occ - 1)
+    return max_absent_days < n_days
 
 
 class Task:
@@ -101,9 +124,16 @@ class Task:
             True if the task can be done by this coloc, False otherwise.
     """
 
-    def __init__(self, name: str):
+    def __init__(
+        self,
+        name: str,
+        weight: Callable[[int, float], tuple[int, float]],
+        possible_within: Callable[[list[bool]], bool],
+    ) -> None:
         """Initialize a task with some standard weight function."""
-        raise NotImplementedError("")
+        self.name = name
+        self.weight = weight
+        self.prossible_within = possible_within
 
     def __str__(self):
         """Return a string representation of the task."""
@@ -112,3 +142,67 @@ class Task:
     def can_be_made_by(self, coloc):
         """Return True if the task can be made by `coloc`."""
         raise NotImplementedError("")
+
+    @classmethod
+    def one_shot_fixed_weight(
+        cls, name: str, total_weight: float, min_prop: float
+    ) -> "Task":
+        """Return a one-shot task with fixed weight."""
+        weight = weight_fun(
+            proportional=False,
+            min_prop=min_prop,
+            min_nb_coloc=1,
+            total_weight=total_weight,
+        )
+        return Task(name=name, weight=weight, possible_within=any)
+
+    @classmethod
+    def one_shot_variable_weight(
+        cls, name: str, weight_per_coloc: float, min_prop: float
+    ) -> "Task":
+        """Return a one-shot task with a weight growing with the number of colocs."""
+        weight = weight_fun(
+            proportional=True,
+            min_prop=min_prop,
+            min_nb_coloc=1,
+            weight_per_coloc=weight_per_coloc,
+        )
+        return Task(name=name, weight=weight, possible_within=any)
+
+    @classmethod
+    def regular_fixed_weight(
+        cls, name: str, total_weight: float, min_prop: float, each_n_days: int
+    ):
+        """Return a regular task with fixed weight."""
+        weight = weight_fun(
+            proportional=False,
+            min_prop=min_prop,
+            min_nb_coloc=1,
+            total_weight=total_weight,
+        )
+        return Task(
+            name=name,
+            weight=weight,
+            possible_within=lambda av_days: less_than_n_consecutive_days_absent(
+                av_days, each_n_days
+            ),
+        )
+
+    @classmethod
+    def regular_variable_weight(
+        cls, name: str, weight_per_coloc: float, min_prop: float, each_n_days: int
+    ) -> "Task":
+        """Return a regular task with a weight growing with the number of colocs."""
+        weight = weight_fun(
+            proportional=False,
+            min_prop=min_prop,
+            min_nb_coloc=1,
+            weight_per_coloc=weight_per_coloc,
+        )
+        return Task(
+            name=name,
+            weight=weight,
+            possible_within=lambda av_days: less_than_n_consecutive_days_absent(
+                av_days, each_n_days
+            ),
+        )
